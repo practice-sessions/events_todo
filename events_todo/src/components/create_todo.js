7533ef9d4bcf1c2297060fe37e16e1a673241bd6
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-09-07 23:47:27 +0100

    Used bind method for binding

--- events_todo/src/components/create_todo.js
+++ events_todo/src/components/create_todo.js
@@ -4,6 +4,11 @@ class Create_Todo extends Component {
   constructor(props) {
     super(props);
 
+    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+    this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
     this.state = {
       todo_description: '',
       todo_responsible: '',
