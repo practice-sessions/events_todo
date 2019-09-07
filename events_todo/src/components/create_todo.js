37762a9a03240ee033d1824e27b5728e0865ebe7
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-09-07 23:35:35 +0100

    Added methods to update the state properties

--- events_todo/src/components/create_todo.js
+++ events_todo/src/components/create_todo.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 
 class Create_Todo extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            todo_description: '',
-            todo_responsible: '',
-            todo_priority: '',
-            todo_completed: false
-        }
-    }
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_completed: false
+    };
+  }
+
+  onChangeTodoDescription(e) {
+    this.setState({
+      todo_description: e.target.value
+    });
+  }
+
+  onChangeTodoResponsible(e) {
+    this.setState({
+      todo_responsible: e.target.value
+    });
+  }
+
+  onChangeTodoPriority(e) {
+    this.setState({
+      todo_priority: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
