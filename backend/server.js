28932316dc288af37523bec93bd8b8f02e5c29f6
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-09-09 22:23:53 +0100

    Added api to find todo items

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db');
 const cors = require('cors');
-const PORT = 4440;
+const connectDB = require('./config/db');
+const todoRoutes = express.Router();
+let Todo = require('./models/Todo');
+
+const PORT = 5555;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/todos', todoRoutes);
 
 //Connect Database
 connectDB();
+
+todoRoutes.route('/').get(function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log('Server is running on Port: ' + PORT);
