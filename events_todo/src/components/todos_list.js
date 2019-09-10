ad44a830438c001e92ac6087c7c45557582e1b06
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-09-10 18:00:51 +0100

    used the component constructor to initialize the state

--- events_todo/src/components/todos_list.js
+++ events_todo/src/components/todos_list.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Todo = props => (
+  <tr>
+    <td>{props.todo.todo_description}</td>
+    <td>{props.todo.todo_responsible}</td>
+    <td>{props.todo.todo_priority}</td>
+    <td>
+      <Link to={'/edit/' + props.todo._id}>Edit</Link>
+    </td>
+  </tr>
+);
 
 class Todos_List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] };
+  }
   render() {
     return (
       <div>
