b51728baf146f78ea04aa64485680f311b09f826
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-09-12 11:09:55 +0100

    Added class component to set the initial state

--- events_todo/src/components/edit_todo.js
+++ events_todo/src/components/edit_todo.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Edit_Todo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_completed: false
+    };
+  }
+
   render() {
     return (
       <div>
