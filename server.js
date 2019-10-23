db9dd047489c5fe1657bbbd31e1f31040db24605
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-23 14:36:54 +0100

    make it better

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const todoRoutes = express.Router();
+let Todo = require('./models/Todo');
+
+const PORT = 5555;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/todos', todoRoutes);
+
+//Connect Database
+connectDB();
+
+todoRoutes.route('/').get(function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRoutes.route('/:id').get(function(req, res) {
+  let id = req.params.id;
+  Todo.findById(id, function(err, todo) {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route('/update/:id').post(function(req, res) {
+  Todo.findById(req.params.id, function(err, todo) {
+    if (!todo) res.status(404).send('data is not found');
+    else todo.todo_description = req.body.todo_description;
+    todo.todo_responsible = req.body.todo_responsible;
+    todo.todo_priority = req.body.todo_priority;
+    todo.todo_completed = req.body.todo_completed;
+
+    todo
+      .save()
+      .then(todo => {
+        res.json('Todo updated!');
+      })
+      .catch(err => {
+        res.status(400).send('Update not possible');
+      });
+  });
+});
+
+todoRoutes.route('/add').post(function(req, res) {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then(todo => {
+      res.status(200).json({ todo: 'todo added successfully' });
+    })
+    .catch(err => {
+      res.status(400).send('adding new todo failed');
+    });
+});
+
+app.listen(PORT, () => {
+  console.log('Server is running on Port: ' + PORT);
+});
