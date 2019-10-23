c87abb058c0d09c6dbac911bec3f8b8f9c41fd1d
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-23 14:41:30 +0100

    make it better

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Create_Todo from './components/Create_Todo';
+import Edit_Todo from './components/Edit_Todo';
+import Todos_List from './components/Todos_List';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(faEdit);
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            {/* <a className='navbar-brand' href='/' target='_blank'>
+              <FontAwesomeIcon icon={['fas', 'edit']} />
+            </a> */}
+
+            <Link to='/' className='navbar-brand'>
+              <FontAwesomeIcon icon={['fas', 'edit']} />
+            </Link>
+
+            <Link to='/' className='navbar-brand'>
+              Todo App
+            </Link>
+            <div className='collpase navbar-collapse'>
+              <ul className='navbar-nav mr-auto'>
+                <li className='navbar-item'>
+                  <Link to='/' className='nav-link'>
+                    Todos
+                  </Link>
+                </li>
+                <li className='navbar-item'>
+                  <Link to='/create' className='nav-link'>
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br />
+          <Route path='/' exact component={Todos_List} />
+          <Route path='/edit/:id' component={Edit_Todo} />
+          <Route path='/create' component={Create_Todo} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
