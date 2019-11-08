235fbc6a98dc8f590a3af320c9fd2393c38c32fb
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-11-08 16:10:12 +0100

    Add delete todo, state and completed date fields totodo table

--- src/components/Todos_List.js
+++ src/components/Todos_List.js
@@ -4,12 +4,27 @@ import axios from 'axios';
 
 const Todo = props => (
   <tr>
-    <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
-        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
-        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
-        <td>
-            <Link to={"/edit/"+props.todo._id}>Edit</Link>
-        </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_description}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_responsible}
+    </td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>
+      {props.todo.todo_priority}
+    </td>
+    <td>
+      <Link to={'/edit/' + props.todo._id}>Edit</Link>
+    </td>
+    <td>
+      <Link to={'/delete/' + props.todo._id}>Delete</Link>
+    </td>
+    <td>
+      <Link to={'/State Date/' + props.todo._id}>State Date</Link>
+    </td>
+    <td>
+      <Link to={'/Completed Date/' + props.todo._id}>Completed Date</Link>
+    </td>
   </tr>
 );
 
@@ -46,7 +61,10 @@ class Todos_List extends Component {
               <th>Description</th>
               <th>Responsible</th>
               <th>Priority</th>
-              <th>Action</th>
+              <th>Edit Todo</th>
+              <th>Delete Todo</th>
+              <th>State Date</th>
+              <th>Completed Date</th>
             </tr>
           </thead>
           <tbody>{this.todoList()}</tbody>
